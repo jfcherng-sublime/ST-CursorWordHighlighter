45feafb5bcd5358d14a25bb77a60caecc417d8a0
Jack Cherng <jfcherng@gmail.com>
2020-09-27 14:04:12 +0800

    feat: support Chinese segmentation
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- CursorWordHighlighter.py
+++ CursorWordHighlighter.py
@@ -1,79 +1,154 @@
-import sublime, sublime_plugin
+import re
+import sublime
+import sublime_plugin
+
+from . import jieba
 
 # Global variables
+chinese_regex_obj = jieba.re_han_default
 search_limit = 20000
 file_size_limit = 4194304
 settings = {}
-word_separators = ''
+word_separators = ""
 highlighter_enabled = True
 case_sensitive = True
 whole_word = True
 draw_outline = True
-color_scope = 'comment'
+color_scope = "comment"
 highlight_on_gutter = False
-gutter_icon_type = ''
+gutter_icon_type = ""
 search_flags = 0
 draw_flags = sublime.DRAW_NO_FILL
 
-color_highlight_scopes = ['entity.name.class', 'support.function', 'variable.parameter', 'invalid.deprecated', 'invalid', 'string']
+color_highlight_scopes = [
+    "entity.name.class",
+    "support.function",
+    "variable.parameter",
+    "invalid.deprecated",
+    "invalid",
+    "string",
+]
 
-def plugin_loaded():
-    get_settings().add_on_change('Preferences-reload', get_settings)
+# 1就在今天進行測試bbb，你好嗎
+# bbbb測試asdbn
+# ggg測試ascxias
+# 測試你好嗎
+# 你好嗎
+# 測試
+# 測試
 
-def get_settings():
+
+def plugin_loaded() -> None:
+    get_settings().add_on_change("Preferences-reload", get_settings)
+    sublime.set_timeout_async(jieba.initialize, 0)
+
+
+def get_settings() -> sublime.Settings:
     global settings, highlighter_enabled, case_sensitive, whole_word, draw_outline, color_scope
     global highlight_on_gutter, gutter_icon_type, search_flags, draw_flags, word_separators
 
-    settings = sublime.load_settings('Preferences.sublime-settings')
-    word_separators = settings.get('word_separators')
-    highlighter_enabled = bool(settings.get('cursor_word_highlighter_enabled', True))
-    case_sensitive = bool(settings.get('cursor_word_highlighter_case_sensitive', True))
-    whole_word = bool(settings.get('cursor_word_highlighter_whole_word', True))
-    draw_outline = bool(settings.get('cursor_word_highlighter_draw_outlined', True))
-    color_scope = settings.get('cursor_word_highlighter_color_scope_name', 'comment')
-    highlight_on_gutter = bool(settings.get('cursor_word_highlighter_mark_occurrences_on_gutter', False))
+    settings = sublime.load_settings("Preferences.sublime-settings")
+    word_separators = settings.get("word_separators")
+    highlighter_enabled = bool(settings.get("cursor_word_highlighter_enabled", True))
+    case_sensitive = bool(settings.get("cursor_word_highlighter_case_sensitive", True))
+    whole_word = bool(settings.get("cursor_word_highlighter_whole_word", True))
+    draw_outline = bool(settings.get("cursor_word_highlighter_draw_outlined", True))
+    color_scope = settings.get("cursor_word_highlighter_color_scope_name", "comment")
+    highlight_on_gutter = bool(
+        settings.get("cursor_word_highlighter_mark_occurrences_on_gutter", False)
+    )
 
-    if highlight_on_gutter :
-        gutter_icon_type = settings.get('cursor_word_highlighter_icon_type_on_gutter', 'dot')
+    if highlight_on_gutter:
+        gutter_icon_type = settings.get("cursor_word_highlighter_icon_type_on_gutter", "dot")
     else:
-        gutter_icon_type = ''
+        gutter_icon_type = ""
 
-    if not case_sensitive :
+    if not case_sensitive:
         search_flags = sublime.IGNORECASE
     else:
         search_flags = 0
 
-    if not draw_outline :
+    if not draw_outline:
         draw_flags = sublime.DRAW_NO_OUTLINE
     else:
         draw_flags = sublime.DRAW_NO_FILL
 
     return settings
 
-class CursorWordHighlighterListener(sublime_plugin.EventListener):
 
-    def on_post_text_command(self, view, command_name, args):
+def get_word_by_point(view: sublime.View, pt: int) -> list:
+    word_region_st = view.word(pt)
+    word_st = view.substr(word_region_st)
+
+    start_point = word_region_st.begin()
+    offset = 0
+    for word in jieba.cut(word_st):
+        word_len = len(word)
+        jieba_word_span = (
+            start_point + offset,
+            start_point + offset + word_len,
+        )
+
+        if jieba_word_span[0] <= pt < jieba_word_span[1]:
+            return [sublime.Region(*jieba_word_span), word]
+
+        offset += word_len
+
+    return [sublime.Region(-1, -1), ""]
+
+
+def get_word_regex(word: str, is_whole_word: bool = False) -> str:
+    if not word:
+        return ""
+
+    regex_l = r"(?<=\W)" if is_whole_word else ""
+    regex_r = r"(?=\W)" if is_whole_word else ""
+
+    if chinese_regex_obj.match(word[0]):
+        regex_l = ""
+
+    if chinese_regex_obj.match(word[-1]):
+        regex_r = ""
+
+    return regex_l + re.escape(word) + regex_r
+
+
+class CursorWordHighlighterListener(sublime_plugin.EventListener):
+    def on_post_text_command(self, view: sublime.View, command_name: str, args: dict) -> None:
         if not highlighter_enabled:
             view.erase_regions("CursorWordHighlighter")
             return
 
-        if view.size() <= file_size_limit :
-            is_limited_size = False
-        else:
-            is_limited_size = True
+        is_limited_size = view.size() > file_size_limit
 
         regions = []
         processedWords = []
         occurrencesMessage = []
         occurrencesCount = 0
-        if command_name == 'drag_select' or command_name == 'move' or (command_name == 'set_motion' and "move" in args['motion']):
+        if (
+            command_name == "drag_select"
+            or command_name == "move"
+            or (command_name == "set_motion" and "move" in args["motion"])
+        ):
             for sel in view.sel():
+                string = ""
+
                 if sel.empty():
-                    string = view.substr(view.word(sel)).strip()
+                    string = get_word_by_point(view, sel.b)[1]
+
+                    if not string.strip():
+                        break
+
                     if string not in processedWords:
                         processedWords.append(string)
                         if string and all([not c in word_separators for c in string]):
-                                regions = self.find_regions(view, regions, string, is_limited_size)
+                            regions = self.find_regions(
+                                view,
+                                regions,
+                                string,
+                                is_limited_size,
+                            )
+
                 elif not sel.empty():
                     word = view.word(sel)
                     if word.end() == sel.end() and word.begin() == sel.begin():
@@ -81,24 +156,33 @@ class CursorWordHighlighterListener(sublime_plugin.EventListener):
                         if string not in processedWords:
                             processedWords.append(string)
                             if string and all([not c in word_separators for c in string]):
-                                    regions = self.find_regions(view, regions, string, is_limited_size)
+                                regions = self.find_regions(view, regions, string, is_limited_size)
 
-                occurrences = len(regions)-occurrencesCount;
+                occurrences = len(regions) - occurrencesCount
                 if occurrences > 0:
-                    occurrencesMessage.append(str(occurrences) + ' occurrence' + ('s' if occurrences != 1 else '') + ' of "' + string + '"')
+                    occurrencesMessage.append(
+                        str(occurrences)
+                        + " occurrence"
+                        + ("s" if occurrences != 1 else "")
+                        + ' of "'
+                        + string
+                        + '"'
+                    )
                     occurrencesCount = occurrencesCount + occurrences
 
             view.erase_regions("CursorWordHighlighter")
             if regions:
-                view.add_regions("CursorWordHighlighter", regions, color_scope, gutter_icon_type, draw_flags)
+                view.add_regions(
+                    "CursorWordHighlighter", regions, color_scope, gutter_icon_type, draw_flags
+                )
             else:
                 view.erase_status("CursorWordHighlighter")
 
-    def find_regions(self, view, regions, string, limited_size):
-        if not whole_word :
-            search = string
-        else :
-            search = r'(?<!\w)'+string+r'(?!\w)'
+    def find_regions(
+        self, view: sublime.View, regions: list, string: str, limited_size: int
+    ) -> list:
+        search = get_word_regex(string, whole_word)
+
         if not limited_size:
             regions = view.find_all(search, search_flags)
         else:
@@ -117,61 +201,84 @@ class CursorWordHighlighterListener(sublime_plugin.EventListener):
                         from_point = region.end()
                 else:
                     break
+
         return regions
 
+
 class PersistentHighlightWordsCommand(sublime_plugin.WindowCommand):
-    def get_words(self, text):
+    def get_words(self, text: str) -> list:
         return text.split()
 
-    def run(self):
+    def run(self) -> None:
         view = self.window.active_view()
+
         if not view:
             return
-        word_list = self.get_words(view.settings().get('cursor_word_highlighter_persistant_highlight_text', ''))
+
+        word_list = self.get_words(
+            view.settings().get("cursor_word_highlighter_persistant_highlight_text", "")
+        )
         cursor_word = ""
-        for region in view.sel():
-            if region.empty() :
-                cursor_word = view.substr(view.word(region)).strip()
-            else :
-                cursor_word = view.substr(region).strip()
+        for sel in view.sel():
+            if sel.empty():
+                cursor_word = get_word_by_point(view, sel.b)[1].strip()
+
+                if not cursor_word:
+                    cursor_word = view.substr(view.word(sel)).strip()
+            else:
+                cursor_word = view.substr(sel).strip()
+
             if cursor_word:
                 if cursor_word in word_list:
                     word_list.remove(cursor_word)
                 else:
                     word_list.append(cursor_word)
                 break
-        display_list = ' '.join(word_list)
+
+        display_list = " ".join(word_list)
         self.highlight(display_list)
 
-    def highlight(self, text):
-        self.window.run_command('persistent_unhighlight_words')
+    def highlight(self, text: str) -> None:
+        self.window.run_command("persistent_unhighlight_words")
+
         view = self.window.active_view()
         words = self.get_words(text)
-        regions = []
         size = 0
         word_set = set()
         for word in words:
             if len(word) < 1 or word in word_set:
                 continue
+
             word_set.add(word)
-            if not whole_word :
-                search = word
-            else :
-                search = r'(?<!\w)'+word+r'(?!\w)'
+
+            search = get_word_regex(word, whole_word)
             regions = view.find_all(search, search_flags)
-            highlightName = 'cursor_word_highlighter_persistant_highlight_word_%d' % size
-            view.add_regions(highlightName, regions, color_highlight_scopes[size % len(color_highlight_scopes)] , '', sublime.DRAW_SOLID_UNDERLINE)
+            highlightName = "cursor_word_highlighter_persistant_highlight_word_%d" % size
+
+            view.add_regions(
+                highlightName,
+                regions,
+                color_highlight_scopes[size % len(color_highlight_scopes)],
+                "",
+                sublime.DRAW_SOLID_UNDERLINE,
+            )
+
             size += 1
-        view.settings().set('cursor_word_highlighter_persistant_highlight_size', size)
-        view.settings().set('cursor_word_highlighter_persistant_highlight_text', text)
+
+        view.settings().set("cursor_word_highlighter_persistant_highlight_size", size)
+        view.settings().set("cursor_word_highlighter_persistant_highlight_text", text)
+
 
 class PersistentUnhighlightWordsCommand(sublime_plugin.WindowCommand):
-    def run(self):
+    def run(self) -> None:
         view = self.window.active_view()
+
         if not view:
             return
-        size = view.settings().get('cursor_word_highlighter_persistant_highlight_size', 0)
+
+        size = view.settings().get("cursor_word_highlighter_persistant_highlight_size", 0)
         for i in range(size):
-            view.erase_regions('cursor_word_highlighter_persistant_highlight_word_%d' % i)
-        view.settings().set('cursor_word_highlighter_persistant_highlight_size', 0)
-        view.settings().erase('cursor_word_highlighter_persistant_highlight_text')
+            view.erase_regions("cursor_word_highlighter_persistant_highlight_word_%d" % i)
+
+        view.settings().set("cursor_word_highlighter_persistant_highlight_size", 0)
+        view.settings().erase("cursor_word_highlighter_persistant_highlight_text")
